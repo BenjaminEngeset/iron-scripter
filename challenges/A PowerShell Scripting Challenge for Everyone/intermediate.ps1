26dc87d8323f655d41f1ce9c3439d58df2005ab6
Benjamin Engeset <beneng@hotmail.no>
2023-01-01 11:33:54 +0100

    Added intermediate scripting challenge for everyone

---
+++ challenges/A PowerShell Scripting Challenge for Everyone/intermediate.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    Gets the files from path specified.
+.DESCRIPTION
+    Gets the files from the path specified including subfolders and displays a result that show the total number of files,
+    the total size of all files, the average file size, the computer name, the date the command was ran and the path that was specified.
+.PARAMETER Path
+    The path to specify such as 'C:\Temp'.
+#>
+
+
+function BeginnerGetFile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        
+        [ValidateScript( { Test-Path -PathType Container $_ } )]
+        [string]$Path
+    )
+    process {
+        try {
+            $Measurement = Get-ChildItem -Path $Path -File -Recurse -ErrorAction Stop | Measure-Object -Property Length -Sum -Average
+            [PSCustomObject]@{
+                Count    = $Measurement.Count
+                Average  = $Measurement.Average
+                Sum      = $Measurement.Sum
+                Date     = Get-Date
+                Computer = $env:Computername
+                Path     = $Path
+            }
+        }
+        catch {
+            Write-Error $_
+        }
+    }
+}
