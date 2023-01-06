09da439e658d6bac195bf361d6c66d19c6a53f8d
Benjamin Engeset <beneng@hotmail.no>
2023-01-06 10:41:03 +0100

    Added beginner a windows feature powershell challenge

---
+++ challenges/a windows feature powershell challenge/beginner.ps1
@@ -0,0 +1,22 @@
+function EnableOrDisableWindowsOptionalFeature {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$FeatureName,
+        
+        [Parameter(Mandatory)]
+        [ValidateSet('Enable', 'Disable')]
+        [string]$Status
+    )   
+    process {
+        if ((Get-WindowsOptionalFeature -FeatureName $FeatureName -Online).State -match "^$($Status)") {
+            return Write-Output $PSStyle.Foreground.Cyan "The feature is already $($Status)d." $PSStyle.Reset
+        }
+        if ($Status -eq 'Enable') {
+            Enable-WindowsOptionalFeature -FeatureName $FeatureName -Online
+        }
+        elseif ($Status -eq 'Disable') {
+            Disable-WindowsOptionalFeature -FeatureName $FeatureName -Online
+        }
+    }
+}
